4985acd4321a5a381e1cb0177227096e9b8d9df5
samir <samir.elanany@microengineering.ca>
2024-12-22 14:09:17 +0200

    added some details

--- app/Shipping_calculator_example.php
+++ app/Shipping_calculator_example.php
@@ -9,6 +9,7 @@ use App\Services\Shipping\DimDivisor;
 use App\Services\Shipping\PackageDimension;
 use App\Services\Shipping\Weight;
 
+// مقاسات العبوه الي بعمل عليها المثال 
 $package = [
     'weight' => 6,
     'dimensions' => [
@@ -18,16 +19,33 @@ $package = [
     ],
 ];
 
+// عملت كلاس يضم جميع المقاسات وفيه فانكشن لزياده العرض وكمان لاختبار هلي العبوات تحتوي علي نفس المقاسات ولا لا
 $packageDimensions = new PackageDimension(
     $package['dimensions']['width'],
     $package['dimensions']['height'],
     $package['dimensions']['length']
 );
 
-$billableWeight = (new BillableWeightCalculatorService())->calculate(
+// هنا وسعت العبوه
+$widerPackageDimensions = $packageDimensions->increaseWidth(10);
+
+// وثبت الوزن في متغير واحد عشان مكررش العباره ديه
+$weight = new Weight($package['weight']);
+
+// وهنا خزنت الكلاس الي بيحسبلي قيمه العبوه في متغير لتسهيل القراءة
+$billClass = new BillableWeightCalculatorService();
+
+$billableWeight = $billClass->calculate(
     $packageDimensions,
-    new Weight($package['weight']),
+    $weight,
+    DimDivisor::FEDEX
+);
+
+$billableWiderWeight = $billClass->calculate(
+    $widerPackageDimensions,
+    $weight,
     DimDivisor::FEDEX
 );
 
 echo $billableWeight . ' lb' . PHP_EOL;
+echo $billableWiderWeight . ' lb' . PHP_EOL;
